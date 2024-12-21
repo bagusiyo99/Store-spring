ed6d1b15acab3d3c47163b760399036bfef8e770
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-12-21 23:11:54 +0700

    Kategori

--- front/src/components/pages/ProductListPage/ProductListPage.js
+++ front/src/components/pages/ProductListPage/ProductListPage.js
@@ -1,34 +1,36 @@
 import React, { useMemo } from "react";
 import FilterIcon from "../../common/FilterIcon";
-// import content from "../../data/content.json";
-// import Categories from "../../Filters/Categories";
+import content from "../../../data/content.json";
 
-// const categories = content?.categories;
+const categories = content?.categories;
 
 const ProductListPage = ({ categoryType }) => {
-  // const categoryContent = useMemo(() => {
-  //   return categories?.find((category) => category.code === categoryType);
-  // }, [categoryType]);
+  // UseMemo untuk mendapatkan kategori berdasarkan categoryType
+  const categoryContent = useMemo(() => {
+    return categories?.find((category) => category.code === categoryType) || {};
+  }, [categoryType]);
 
   return (
     <div>
       <div className="flex">
+        {/* Sidebar untuk filter */}
         <div className="w-[20%] p-[10px] border rounded-lg m-[20px]">
-          <div className="flex justify-between ">
+          <div className="flex justify-between">
             <p className="text-[16px] text-gray-600">Filter</p>
             <FilterIcon />
           </div>
 
           <div>
-            {/* Product types */}
+            {/* List Kategori */}
             <p className="text-[16px] text-black mt-5">Categories</p>
-            {/* <Categories types={categoryContent?.types} /> */}
-
-            <hr></hr>
+            <hr />
           </div>
         </div>
 
-        <div className="p-[15px]"></div>
+        {/* Konten utama */}
+        <div className="p-[40px]">
+          <p>{categoryContent?.description || "Category not found"}</p>
+        </div>
       </div>
     </div>
   );
