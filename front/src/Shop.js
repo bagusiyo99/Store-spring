bc7d9087a964a3824fbde33524173ee4160e2849
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-12-21 16:55:28 +0700

    Kategori

--- front/src/Shop.js
+++ front/src/Shop.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import HeroSection from "./components/HeroSection/HeroSection";
+import NewArrivals from "./components/Section/NewArrivals";
+import Category from "./components/Section/Categories/Category";
+import content from "./data/content.json";
 
+// import Footer from "./components/Footer/Footer";
 const Shop = () => {
   return (
     <>
       <Navigation />
       <HeroSection />
+      <NewArrivals />
+      <Category
+        title={content?.categories[0]?.title}
+        data={content?.categories[0]?.data}
+      />
+      {/* <Footer content={content?.footer} /> */}
     </>
   );
 };
