061371d1dbe3359f93ce96ff0facb647007d0c8c
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-12-23 15:47:41 +0700

    backend

--- backendstore/src/main/java/com/store/backendstore/BackendstoreApplication.java
+++ backendstore/src/main/java/com/store/backendstore/BackendstoreApplication.java
@@ -1,13 +1,13 @@
-package com.store.backend;
+package com.store.backendstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendstoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendstoreApplication.class, args);
 	}
 
 }
--- backendstore/src/test/java/com/store/backendstore/BackendstoreApplicationTests.java
+++ backendstore/src/test/java/com/store/backendstore/BackendstoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.store.backend;
+package com.store.backendstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendstoreApplicationTests {
 
 	@Test
 	void contextLoads() {
