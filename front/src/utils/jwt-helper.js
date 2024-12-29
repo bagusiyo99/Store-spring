7c50b7790cd1ce969248ee674d2d28a1163bf725
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-12-29 20:31:36 +0700

    front LOGIN

--- front/src/utils/jwt-helper.js
+++ front/src/utils/jwt-helper.js
@@ -1,29 +1,29 @@
-// import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
-// export const isTokenValid = ()=>{
-//     const token = localStorage.getItem('authToken');
-//     if (!token) return false;
+export const isTokenValid = () => {
+  const token = localStorage.getItem("authToken");
+  if (!token) return false;
 
-//     try {
-//         const decoded = jwtDecode(token);
-//         const currentTime = Date.now() / 1000; // Current time in seconds
+  try {
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000; // Current time in seconds
 
-//         // Check if the token is expired
-//         return decoded.exp > currentTime;
-//     } catch (error) {
-//         console.error("Invalid token", error);
-//         return false;
-//     }
-// }
+    // Check if the token is expired
+    return decoded.exp > currentTime;
+  } catch (error) {
+    console.error("Invalid token", error);
+    return false;
+  }
+};
 
-// export const saveToken = (token) =>{
-//     localStorage.setItem('authToken',token);
-// }
+export const saveToken = (token) => {
+  localStorage.setItem("authToken", token);
+};
 
-// export const logOut = ()=>{
-//     localStorage.removeItem('authToken');
-// }
+export const logOut = () => {
+  localStorage.removeItem("authToken");
+};
 
-// export const getToken = ()=>{
-//     return localStorage.getItem('authToken');
-// }
+export const getToken = () => {
+  return localStorage.getItem("authToken");
+};
